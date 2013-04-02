50c16c79b70a011cf6c8d0c2e7a9089bf441c695
Will Sobel <will@systeminsights.com>
2013-04-01 21:49:53 -0700

    Initial Commit - Complete Lab

---
+++ AdapterLabTests/ConditionTests.cs
@@ -0,0 +1,202 @@
+﻿/*
+ * Copyright Copyright 2012, System Insights, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdapterLabTests
+{
+    using MTConnect;
+
+    [TestClass]
+    public class ConditionTests
+    {
+        Condition c = new Condition("c");
+        Condition s = new Condition("s", true);
+
+        [TestMethod]
+        public void should_require_a_newline()
+        {
+            Assert.AreEqual(true, c.NewLine);
+        }
+
+        [TestMethod]
+        public void should_include_newly_added_activations()
+        {
+            c.Begin();
+            c.Add(Condition.Level.WARNING, "text", "code");
+            c.Prepare();
+            List<DataItem> list = c.ItemList();
+
+            Assert.AreEqual(1, list.Count);
+            Assert.AreEqual("c|WARNING|text|code||", list[0].ToString());
+        }
+
+        [TestMethod]
+        public void should_remove_old_activations_after_cleanup()
+        {
+            c.Begin();
+            c.Add(Condition.Level.WARNING, "text", "code");
+            c.Prepare();
+            c.Cleanup();
+
+            c.Begin();
+            c.Prepare();
+            List<DataItem> list = c.ItemList();
+            c.Cleanup();
+
+            Assert.AreEqual(1, list.Count);
+            Assert.AreEqual("c|NORMAL||||", list[0].ToString());
+
+            list = c.ItemList(true);
+            Assert.AreEqual(1, list.Count);
+        }
+
+        [TestMethod]
+        public void should_remove_old_activations_and_add_new()
+        {
+            c.Begin();
+            c.Add(Condition.Level.WARNING, "text", "code");
+            c.Prepare();
+            c.Cleanup();
+
+            c.Begin();
+            c.Add(Condition.Level.WARNING, "text", "code2");
+            c.Prepare();
+            List<DataItem> list = c.ItemList();
+            c.Cleanup();
+
+            Assert.AreEqual(2, list.Count);
+            Assert.AreEqual("c|NORMAL||code||", list[0].ToString());
+            Assert.AreEqual("c|WARNING|text|code2||", list[1].ToString());
+        }
+
+        [TestMethod]
+        public void should_remove_only_old_activations()
+        {
+            c.Begin();
+            c.Add(Condition.Level.WARNING, "text", "code1");
+            c.Add(Condition.Level.FAULT, "text", "code2");
+            c.Prepare();
+            List<DataItem> list = c.ItemList();
+            c.Cleanup();
+
+            Assert.AreEqual(2, list.Count);
+
+            c.Begin();
+            c.Add(Condition.Level.FAULT, "text", "code2");
+            c.Prepare();
+            list = c.ItemList();
+            c.Cleanup();
+
+            Assert.AreEqual(1, list.Count);
+            Assert.AreEqual("c|NORMAL||code1||", list[0].ToString());
+            list = c.ItemList(true);
+            Assert.AreEqual(1, list.Count);
+            Assert.AreEqual("c|FAULT|text|code2||", list[0].ToString());
+
+            c.Begin();
+            c.Add(Condition.Level.FAULT, "text", "code2");
+            c.Prepare();
+            list = c.ItemList();
+            c.Cleanup();
+
+            Assert.AreEqual(0, list.Count);
+
+            list = c.ItemList(true);
+            Assert.AreEqual(1, list.Count);
+            Assert.AreEqual("c|FAULT|text|code2||", list[0].ToString());
+
+            c.Begin();
+            c.Prepare();
+            list = c.ItemList();
+            c.Cleanup();
+
+            Assert.AreEqual(1, list.Count);
+            Assert.AreEqual("c|NORMAL||||", list[0].ToString());
+        }
+
+
+        [TestMethod]
+        public void simple_conditions_should_not_mark_and_sweep()
+        {
+            s.Begin();
+            s.Add(Condition.Level.WARNING, "text", "code");
+            s.Prepare();
+            s.Cleanup();
+
+            s.Begin();
+            s.Prepare();
+            List<DataItem> list = s.ItemList();
+            s.Cleanup();
+
+            Assert.AreEqual(0, list.Count);
+
+            list = s.ItemList(true);
+            Assert.AreEqual(1, list.Count);
+            Assert.AreEqual("s|WARNING|text|code||", list[0].ToString());
+        }
+
+        [TestMethod]
+        public void simple_conditions_should_be_cleared_manually()
+        {
+            s.Begin();
+            s.Add(Condition.Level.WARNING, "text", "code");
+            s.Prepare();
+            List<DataItem> list = s.ItemList();
+            s.Cleanup();
+
+            Assert.AreEqual(1, list.Count);
+            Assert.AreEqual("s|WARNING|text|code||", list[0].ToString());
+
+            s.Clear("code");
+            list = s.ItemList(true);
+            Assert.AreEqual(1, list.Count);
+            Assert.AreEqual("s|NORMAL||||", list[0].ToString());
+        }
+
+        [TestMethod]
+        public void simple_conditions_should_clear_one_code_when_multiple_are_present()
+        {
+            s.Begin();
+            s.Add(Condition.Level.WARNING, "text", "code1");
+            s.Add(Condition.Level.FAULT, "text", "code2");
+            s.Prepare();
+            List<DataItem> list = s.ItemList();
+            s.Cleanup();
+
+            Assert.AreEqual(2, list.Count);
+            Assert.AreEqual("s|WARNING|text|code1||", list[0].ToString());
+            Assert.AreEqual("s|FAULT|text|code2||", list[1].ToString());
+
+            s.Begin();
+            s.Clear("code1");
+            list = s.ItemList();
+            s.Prepare();
+            s.Cleanup();
+
+            Assert.AreEqual(1, list.Count);
+            Assert.AreEqual("s|NORMAL||code1||", list[0].ToString());
+
+            list = s.ItemList(true);
+            Assert.AreEqual(1, list.Count);
+            Assert.AreEqual("s|FAULT|text|code2||", list[0].ToString());
+        }
+    }
+}
---
+++ MachineTool.cs
@@ -0,0 +1,205 @@
+﻿/*
+ * Copyright Copyright 2012, System Insights, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AdapterLab
+{
+    using MTConnect;
+    using NAudio;
+    using NAudio.Wave;
+
+    public partial class MachineTool : Form
+    {
+        Adapter mAdapter = new Adapter();
+
+        Event mMode = new Event("mode");
+        Event mExec = new Event("exec");
+
+        Event mAvail = new Event("avail");
+        Event mEStop = new Event("estop");
+        Sample mPosition = new Sample("position");
+        Sample mLoad = new Sample("load");
+
+        Condition mSystem = new Condition("system");
+        Condition mCoolant = new Condition("cool_low", true);
+
+        Message mMessage = new Message("message");
+
+        Event mProgram = new Event("program");
+
+        TimeSeries mAudio = new TimeSeries("audio", 1000);
+        WaveIn mWave;
+        
+        public MachineTool()
+        {
+            InitializeComponent();
+            stop.Enabled = false;
+
+            mAdapter.AddDataItem(mAvail);
+            mAvail.Value = "AVAILABLE";
+
+            mAdapter.AddDataItem(mEStop);
+            mAdapter.AddDataItem(mPosition);
+            mAdapter.AddDataItem(mLoad);
+
+            mAdapter.AddDataItem(mSystem);
+            mSystem.Normal();
+
+            mAdapter.AddDataItem(mCoolant);
+            mCoolant.Normal();
+
+            mAdapter.AddDataItem(mMessage);
+            mAdapter.AddDataItem(mProgram);
+            mAdapter.AddDataItem(mMode);
+            mAdapter.AddDataItem(mExec);
+
+            int count = WaveIn.DeviceCount;
+            for (int dev = 0; dev < count; dev++)
+            {
+                WaveInCapabilities deviceInfo = WaveIn.GetCapabilities(dev);
+                Console.WriteLine("Device {0}: {1}, {2} channels",
+                    dev, deviceInfo.ProductName, deviceInfo.Channels);
+            }
+
+            mWave = new WaveIn();
+            mWave.DeviceNumber = 0;
+            mWave.WaveFormat = new WaveFormat(1000, 1);
+            mWave.DataAvailable += waveIn_DataAvailable;
+
+            mAdapter.AddDataItem(mAudio);
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            // Start the adapter lib with the port number in the text box
+            mAdapter.Port = Convert.ToInt32(port.Text);
+            mAdapter.Start();
+
+            // Disable start and enable stop.
+            start.Enabled = false;
+            stop.Enabled = true;
+
+            // Start our periodic timer
+            gather.Interval = 1000;
+            gather.Enabled = true;
+
+            mWave.StartRecording();
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            // Stop everything...
+            mAdapter.Stop();
+            stop.Enabled = false;
+            start.Enabled = true;
+            gather.Enabled = false;
+
+            mWave.StopRecording();
+        }
+
+        private void gather_Tick(object sender, EventArgs e)
+        {
+            mAdapter.Begin();
+
+            if (estop.Checked)
+                mEStop.Value = "TRIGGERED";
+            else
+                mEStop.Value = "ARMED";
+
+            mPosition.Value = position.Value;
+
+            if (something.Checked)
+                mSystem.Add(Condition.Level.FAULT, "You AK AK is not behaving.", "AKAK", "HIGH");
+            if (flazBat.Checked)
+                mSystem.Add(Condition.Level.WARNING, "You flaz bat needs attention", "FLAZBAT");
+
+            if (automatic.Checked)
+                mMode.Value = "AUTOMATIC";
+            else if (mdi.Checked)
+                mMode.Value = "MANUAL_DATA_INPUT";
+            else
+                mMode.Value = "MANUAL";
+
+            if (running.Checked)
+                mExec.Value = "ACTIVE";
+            else if (feedhold.Checked)
+                mExec.Value = "FEED_HOLD";
+            else
+                mExec.Value = "READY";
+
+            if (messageCode.Text.Length > 0)
+            {
+                mMessage.Value = messageText.Text;
+                mMessage.Code = messageCode.Text;
+            }
+
+            mProgram.Value = program.Text;
+
+            mAdapter.SendChanged();
+        }
+
+        private void load_Scroll(object sender, ScrollEventArgs e)
+        {
+            loadValue.Text = load.Value.ToString();
+            mLoad.Value = load.Value;
+            mAdapter.SendChanged();
+        }
+
+        private void position_Scroll(object sender, ScrollEventArgs e)
+        {
+            positionValue.Text = position.Value.ToString();
+        }
+
+        private void coolant_CheckedChanged(object sender, EventArgs e)
+        {
+            if (coolant.Checked)
+                mCoolant.Add(Condition.Level.WARNING, "You coolant is low", "COOLLOW");
+            else
+                mCoolant.Clear("COOLLOW");
+
+            mAdapter.SendChanged();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CuttingTool toolWindow = new CuttingTool(mAdapter);
+            toolWindow.Show(this);
+        }
+
+        void waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            double[] samples = new double[e.BytesRecorded / 2];
+            for (int index = 0; index < e.BytesRecorded; index += 2)
+            {
+                short sample = (short)((e.Buffer[index + 1] << 8) |
+                                        e.Buffer[index + 0]);
+                float f = sample / 32768f;
+                samples[index / 2] = f;
+            }
+
+            mAudio.Values = samples;
+            mAdapter.SendChanged();
+        }
+     }
+}
