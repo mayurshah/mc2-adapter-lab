fcb2694e91d0ac60a6b43e736bd5cccfb3096909
Will Sobel <will@systeminsights.com>
2013-04-01 22:07:02 -0700

    Clean project at step 1.

--- MachineTool.cs
+++ MachineTool.cs
@@ -32,24 +32,7 @@ namespace AdapterLab
     public partial class MachineTool : Form
     {
         Adapter mAdapter = new Adapter();
-
-        Event mMode = new Event("mode");
-        Event mExec = new Event("exec");
-
         Event mAvail = new Event("avail");
-        Event mEStop = new Event("estop");
-        Sample mPosition = new Sample("position");
-        Sample mLoad = new Sample("load");
-
-        Condition mSystem = new Condition("system");
-        Condition mCoolant = new Condition("cool_low", true);
-
-        Message mMessage = new Message("message");
-
-        Event mProgram = new Event("program");
-
-        TimeSeries mAudio = new TimeSeries("audio", 1000);
-        WaveIn mWave;
         
         public MachineTool()
         {
@@ -58,36 +41,6 @@ namespace AdapterLab
 
             mAdapter.AddDataItem(mAvail);
             mAvail.Value = "AVAILABLE";
-
-            mAdapter.AddDataItem(mEStop);
-            mAdapter.AddDataItem(mPosition);
-            mAdapter.AddDataItem(mLoad);
-
-            mAdapter.AddDataItem(mSystem);
-            mSystem.Normal();
-
-            mAdapter.AddDataItem(mCoolant);
-            mCoolant.Normal();
-
-            mAdapter.AddDataItem(mMessage);
-            mAdapter.AddDataItem(mProgram);
-            mAdapter.AddDataItem(mMode);
-            mAdapter.AddDataItem(mExec);
-
-            int count = WaveIn.DeviceCount;
-            for (int dev = 0; dev < count; dev++)
-            {
-                WaveInCapabilities deviceInfo = WaveIn.GetCapabilities(dev);
-                Console.WriteLine("Device {0}: {1}, {2} channels",
-                    dev, deviceInfo.ProductName, deviceInfo.Channels);
-            }
-
-            mWave = new WaveIn();
-            mWave.DeviceNumber = 0;
-            mWave.WaveFormat = new WaveFormat(1000, 1);
-            mWave.DataAvailable += waveIn_DataAvailable;
-
-            mAdapter.AddDataItem(mAudio);
         }
 
         private void start_Click(object sender, EventArgs e)
@@ -103,8 +56,6 @@ namespace AdapterLab
             // Start our periodic timer
             gather.Interval = 1000;
             gather.Enabled = true;
-
-            mWave.StartRecording();
         }
 
         private void stop_Click(object sender, EventArgs e)
@@ -114,56 +65,15 @@ namespace AdapterLab
             stop.Enabled = false;
             start.Enabled = true;
             gather.Enabled = false;
-
-            mWave.StopRecording();
         }
 
         private void gather_Tick(object sender, EventArgs e)
         {
-            mAdapter.Begin();
-
-            if (estop.Checked)
-                mEStop.Value = "TRIGGERED";
-            else
-                mEStop.Value = "ARMED";
-
-            mPosition.Value = position.Value;
-
-            if (something.Checked)
-                mSystem.Add(Condition.Level.FAULT, "You AK AK is not behaving.", "AKAK", "HIGH");
-            if (flazBat.Checked)
-                mSystem.Add(Condition.Level.WARNING, "You flaz bat needs attention", "FLAZBAT");
-
-            if (automatic.Checked)
-                mMode.Value = "AUTOMATIC";
-            else if (mdi.Checked)
-                mMode.Value = "MANUAL_DATA_INPUT";
-            else
-                mMode.Value = "MANUAL";
-
-            if (running.Checked)
-                mExec.Value = "ACTIVE";
-            else if (feedhold.Checked)
-                mExec.Value = "FEED_HOLD";
-            else
-                mExec.Value = "READY";
-
-            if (messageCode.Text.Length > 0)
-            {
-                mMessage.Value = messageText.Text;
-                mMessage.Code = messageCode.Text;
-            }
-
-            mProgram.Value = program.Text;
-
-            mAdapter.SendChanged();
         }
 
         private void load_Scroll(object sender, ScrollEventArgs e)
         {
             loadValue.Text = load.Value.ToString();
-            mLoad.Value = load.Value;
-            mAdapter.SendChanged();
         }
 
         private void position_Scroll(object sender, ScrollEventArgs e)
@@ -173,12 +83,6 @@ namespace AdapterLab
 
         private void coolant_CheckedChanged(object sender, EventArgs e)
         {
-            if (coolant.Checked)
-                mCoolant.Add(Condition.Level.WARNING, "You coolant is low", "COOLLOW");
-            else
-                mCoolant.Clear("COOLLOW");
-
-            mAdapter.SendChanged();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -189,17 +93,6 @@ namespace AdapterLab
 
         void waveIn_DataAvailable(object sender, WaveInEventArgs e)
         {
-            double[] samples = new double[e.BytesRecorded / 2];
-            for (int index = 0; index < e.BytesRecorded; index += 2)
-            {
-                short sample = (short)((e.Buffer[index + 1] << 8) |
-                                        e.Buffer[index + 0]);
-                float f = sample / 32768f;
-                samples[index / 2] = f;
-            }
-
-            mAudio.Values = samples;
-            mAdapter.SendChanged();
         }
      }
 }
